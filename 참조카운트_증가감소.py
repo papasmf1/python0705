d59bcb738132764b15be9a87da0f86a6ad1b8906
papasmf1 <papasmf1@gmail.com>
2021-07-07 11:03:16 +0900

    클래스연습2

--- 참조카운트_증가감소.py
+++ 참조카운트_증가감소.py
@@ -1,13 +1,18 @@
+import sys 
+
 class MyClass:
+    #초기화 메서드(생성자)
     def __init__(self, value):
         self.Value = value 
-        print("Class is created! Value = ", value)
-        
+        print("Instance is created! Value = ", value)
+    #소멸자 메서드 
     def __del__(self):
-        print("Class is deleted!")
+        print("Instance is deleted!")
         
-
+#인스턴스 생성
 c = MyClass(10)
-c_copy = c
-del c 
-del c_copy
+#참고 카운트를 출력
+#print("참조 카운트:", sys.getrefcount(c) )
+#del c 
+
+print("전체 코드 실행 종료")
